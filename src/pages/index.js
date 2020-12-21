4b706dc4f800fd13d458ee4895b42e21d0cc864c
Zeynep Özdem Özdem <zeynebozdem@gmail.com>
2020-12-21 23:39:59 +0300

    photo query created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import {graphql} from "gatsby"
+import Img from "gatsby-image"
+import './index.css'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = ({data}) => {
+ console.log(data);
+ return <div>
+   <div className="photo">
+   {data.allFile.edges.map(({node}) =>{
+     return <Img key={node.id} fluid={node.childImageSharp.fluid}/>
+   })}
+   </div>
+   
+   
+  </div>
+}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export const query = graphql`
+  {
+  allFile(filter:{absolutePath: {regex: "//photos//"}}){
+    edges{
+      node{
+        id
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
